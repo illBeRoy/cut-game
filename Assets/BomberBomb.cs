cd9d09838ad9aa2e6943ad75df3c7ba178242dff
GuyKroizman <kroizguy@gmail.com>
2019-01-25 21:32:44 +0200

    adsfadsf

---
+++ Assets/BomberBomb.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BomberBomb : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/BomberEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberEnemy : MonoBehaviour
+{
+    public float speed = 4;
+    public float bombDroppingInterval = 1;
+
+    public GameObject explosion;
+    public GameObject bomb;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+
+        StartCoroutine("DropBombSometimes");
+    }
+
+    void Update()
+    {
+        Fly();
+
+    }
+    
+    IEnumerator DropBombSometimes()
+    {
+        for(; ; )
+        {
+            yield return new WaitForSeconds(bombDroppingInterval);
+            DropBomb();
+        }
+    }
+
+    private void DropBomb()
+    {
+        
+        var bombClone = Instantiate(bomb, new Vector2(transform.position.x, transform.position.y - 0.8f), transform.rotation);
+        bombClone.transform.Rotate(0, 0, 180);
+    }
+
+    private void Fly()
+    {
+        float hieght = Mathf.PingPong(Time.timeSinceLevelLoad, 2f) - 1f;
+        var destination = new Vector2(target.position.x, transform.position.y + (hieght * 3));
+        transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+    }
+}
