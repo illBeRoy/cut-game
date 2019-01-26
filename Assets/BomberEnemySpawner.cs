f012e5bb90e2999ac8d7ebc21d55f40902343441
GuyKroizman <kroizguy@gmail.com>
2019-01-26 14:07:34 +0200

    spawn-bomber

---
+++ Assets/BomberEnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BomberEnemySpawner : MonoBehaviour
+{
+    public BomberEnemy enemy;
+    public float spawnInterval = 4;
+
+    void Start()
+    {
+        StartCoroutine("SpawnEnemySometimes");
+        
+    }
+
+    IEnumerator SpawnEnemySometimes()
+    {
+        for(; ; )
+        {
+            var enemyClone = Instantiate<BomberEnemy>(enemy, transform.position, transform.rotation);
+
+            enemyClone.bombDroppingInterval = Random.Range(1.5f, 5);
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void Update()
+    {
+    }
+}
