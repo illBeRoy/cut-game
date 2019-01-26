38eb01f86015c6d0dd758a067e9fab567a5d74d1
Roy Sommer <roy@sommer.co.il>
2019-01-26 16:26:42 +0200

    cloud effect

---
+++ Assets/RepeatingMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingMove : MonoBehaviour
+{
+    public float width;
+    public float speed = 3;
+
+    private float startingPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.startingPoint = this.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        if (position.x < this.startingPoint - this.width) {
+            position.x += this.width;
+        }
+        position.x -= this.speed * Time.deltaTime;
+        this.transform.position = position;
+    }
+}
