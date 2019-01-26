27f573aff4a407d25326535fcbc0e0da70cd4559
Roy Sommer <roy@sommer.co.il>
2019-01-26 16:08:54 +0200

    updated follower to change facing direction. added zombie lady

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -15,6 +15,14 @@ public class Follower : MonoBehaviour
 
     void Update()
     {
+        Vector3 localScale = this.transform.localScale;
+        if (this.target.position.x < this.transform.position.x) {
+            localScale.x = -Mathf.Abs(localScale.x);
+        } else {
+            localScale.x = Mathf.Abs(localScale.x);
+        }
+        this.transform.localScale = localScale;
+
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
 
         if(Vector2.Distance(target.transform.position, transform.position) < triggerDistance)
